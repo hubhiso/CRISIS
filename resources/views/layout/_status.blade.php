76bca8b4f86053d1e48b295c322436de9ba77c43
Noppon Kumpdetch <>
2021-11-10 23:03:56 +0700

    update 	- status

--- resources/views/layout/_status.blade.php
+++ resources/views/layout/_status.blade.php
@@ -1,98 +1,132 @@
-<? 
-$step=1;
+<?php 
+    $step=1;
 
-//$step=2;
-//$step=3;
-$thaimonth = ["","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค."];
-
+    //$step=2;
+    //$step=3;
 ?>
-{{-- @foreach($data as $Semidata)
- {{ $Semidata }} 
-@endforeach --}}
 
-<div class="timeline-item">
-
-
-@foreach($data as $Semidata)
-    
-    <input type="hidden" id="fcase" value="{{$Semidata->case_id}}">
-    {{-- @if ($step==1) --}}
-    @if ($step == 1)
-
-        @if($Semidata->operate_status == 1 )
-
-        <div class="timeline-icon">
-        <img src="images/markx40.png" alt="">
-        </div>
-        <div class="timeline-content">
-            <h2> {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}}-{{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}}{{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}}  </h2>
-            <p>
-                ส่งเรื่องสำเร็จ
-            </p>
-
-        </div>
-        @else
-        <div class="timeline-icon">
-        <img src="images/minusx40.png" alt="">
-        </div>
-            <div class="timeline-content">
-                <h2>  </h2>
-                <p>
-                ส่งเรื่องสำเร็จ
-                </p>
-            </div>
-        @endif
-       
-        <? //$step++; ?>   
-
-</div>
-    
-    
-    @endif
-
-   
-@if ($step == 2)
-<div class="timeline-item">
-        @if($Semidata->operate_status == 2 )
-            <div class="timeline-icon">
-            <img src="images/markx40.png" alt="">
-            </div>
-                <div class="timeline-content right">
-                    <h2> {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}}-{{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}}{{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}} </h2>
-                    <p>
-                    เจ้าหน้าที่รับเรื่องแล้ว
-                    </p>
-                </div>
-        @else
-              <div class="timeline-icon">
-              <img src="images/minusx40.png" alt="">
-              </div>
-                    <div class="timeline-content right">
-                        <h2>  </h2>
-                        <p>
-                        เจ้าหน้าที่รับเรื่องแล้ว
-                        </p>
-                    </div>
-                    
-        @endif
-        <? //$step++; ?>
-</div>
-
-
-  
+@if(Config::get('app.locale') == 'en')
+    <?php 
+        $thaimonth = ["","Jan.","Feb.","Mar.","Apr.","May","Jun.","Jul.","Jul.","Sep.","Oct.","Nov.","Dec."];
+    ?>
+@elseif(Config::get('app.locale') == 'th')
+    <?php 
+        $thaimonth = ["","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค."];
+    ?>
 @endif
 
-{{-- @if ($step==3) --}}
-@if ($step==3)   
-<div class="timeline-item">
+{{-- @foreach($data as $Semidata)
+ {{ $Semidata }}
+@endforeach --}}
+
+<div class="timeline">
+    <br>
+</div>
+
+@foreach($data as $Semidata)
+
+    <input type="hidden" id="fcase" value="{{$Semidata->case_id}}">
+    {{-- @if ($step==1) --}}
+   
+    @if ($step == 1)
+
+    <div class="timeline-item">
+
+            @if($Semidata->operate_status == 1 )
+                <div class="timeline-icon">
+                    <img src="images/markx40.png" alt="">
+                </div>
+                <div class="timeline-content">
+                    <h2> 
+                        @if(Config::get('app.locale') == 'en')
+                            {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))}}
+                        @elseif(Config::get('app.locale') == 'th')
+                            {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}}
+                        @endif
+                    </h2>
+                    <p>
+                        {{ trans('message.tx_status1') }}
+                    </p>
+
+                </div>
+            @else
+                <div class="timeline-icon">
+                    <img src="images/minusx40.png" alt="">
+                </div>
+                <div class="timeline-content">
+                    <h2> </h2>
+                    <p>
+                        {{ trans('message.tx_status1') }}
+                    </p>
+                </div>
+            @endif
+
+        <? //$step++; ?>
+
+    </div>
+
+
+    @endif
+
+
+    @if ($step == 2)
+
+    <div class="timeline-item">
+
+        @if($Semidata->operate_status == 2 )
+        
+            <div class="timeline-icon">
+                <img src="images/markx40.png" alt="">
+            </div>
+            
+            <div class="timeline-content right">
+                <h2> 
+                    @if(Config::get('app.locale') == 'en')
+                        {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))}}
+                    @elseif(Config::get('app.locale') == 'th')
+                        {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}}
+                    @endif
+                </h2>
+                <p>
+                    {{ trans('message.tx_status2_1') }}
+                </p>
+            </div>
+        @else
+        <div class="timeline-icon">
+            <img src="images/minusx40.png" alt="">
+        </div>
+        <div class="timeline-content right">
+            <h2> </h2>
+            <p>
+                {{ trans('message.tx_status2_1') }}
+            </p>
+        </div>
+
+        @endif
+        <? //$step++; ?>
+    </div>
+
+
+    @endif
+
+    {{-- @if ($step==3) --}}
+
+    @if ($step==3)
+    <div class="timeline-item">
         @if($Semidata->operate_status == 3 )
         <div class="timeline-icon">
-        <img src="images/markx40.png" alt="">
+            <img src="images/markx40.png" alt="">
         </div>
         <div class="timeline-content">
-            <h2> {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}}-{{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}}{{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}} </h2>
+            <h2> 
+                @if(Config::get('app.locale') == 'en')
+                    {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))}}
+                @elseif(Config::get('app.locale') == 'th')
+                    {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}}
+                @endif
+            </h2>
             <p>
-            เจ้าหน้าที่สอบถามข้อมูลเพิ่มเติมแล้ว
+                {{ trans('message.tx_status3_1') }}
             </p>
         </div>
         @else
@@ -102,27 +136,33 @@ $thaimonth = ["","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","�
         <div class="timeline-content">
             <h2> - </h2>
             <p>
-            เจ้าหน้าที่สอบถามข้อมูลเพิ่มเติมแล้ว
+                {{ trans('message.tx_status3_1') }}
             </p>
         </div>
-       
+
+        @endif
+        <? //$step++; ?>
+    </div>
+
     @endif
-    <? //$step++; ?>    
-</div>
-
-@endif
 
 
-@if ($step==4)   
-<div class="timeline-item">
+    @if ($step==4)
+    <div class="timeline-item">
         @if($Semidata->operate_status == 4 )
         <div class="timeline-icon">
-        <img src="images/markx40.png" alt="">
+            <img src="images/markx40.png" alt="">
         </div>
-        <div class="timeline-content">
-            <h2> {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}}-{{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}}{{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}} </h2>
+        <div class="timeline-content right">
+            <h2> 
+                @if(Config::get('app.locale') == 'en')
+                    {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))}}
+                @elseif(Config::get('app.locale') == 'th')
+                    {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}}
+                @endif
+            </h2>
             <p>
-            อยู่ระหว่างดำเนินการช่วยเหลือ
+                {{ trans('message.tx_status4_1') }}
             </p>
         </div>
         @else
@@ -130,29 +170,35 @@ $thaimonth = ["","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","�
             <img src="images/minusx40.png" alt="">
         </div>
         <div class="timeline-content">
-            <h2>  - </h2>
+            <h2> - </h2>
             <p>
-            อยู่ระหว่างดำเนินการช่วยเหลือ
+                {{ trans('message.tx_status4_1') }}
             </p>
         </div>
-       
+
+        @endif
+        <? //$step++; ?>
+    </div>
+
     @endif
-    <? //$step++; ?>    
-</div>
-
-@endif
 
 
-@if ($step==5)   
-<div class="timeline-item">
+    @if ($step==5)
+    <div class="timeline-item">
         @if($Semidata->operate_status == 5 or $Semidata->operate_status == 6 )
         <div class="timeline-icon">
-        <img src="images/markx40.png" alt="">
+            <img src="images/markx40.png" alt="">
         </div>
         <div class="timeline-content">
-            <h2> {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}}-{{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}}{{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}} </h2>
+            <h2> 
+                @if(Config::get('app.locale') == 'en')
+                    {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))}}
+                @elseif(Config::get('app.locale') == 'th')
+                    {{date('d',strtotime(str_replace('-','/', $Semidata->operate_time)))}} {{$thaimonth[date('n',strtotime(str_replace('-','/', $Semidata->operate_time)))]}} {{date("Y",strtotime(str_replace('-','/', $Semidata->operate_time)))+543}}
+                @endif        
+            </h2>
             <p>
-             ดำเนินการเสร็จสิ้น
+                {{ trans('message.tx_status5_1') }}
             </p>
         </div>
         @else
@@ -160,87 +206,88 @@ $thaimonth = ["","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","�
             <img src="images/minusx40.png" alt="">
         </div>
         <div class="timeline-content">
-            <h2>  - </h2>
+            <h2> - </h2>
             <p>
-                ดำเนินการเสร็จสิ้น
+                {{ trans('message.tx_status5_1') }}
             </p>
         </div>
-       
+
+        @endif
+        <? //$step++; ?>
+
     @endif
-    <? //$step++; ?>    
-
-@endif
 
 
-<? $step++; ?>   
- 
+    <?php $step++; ?>
+
 @endforeach
-<div class="timeline-item">
 
-@if ($step==1) 
+    <div class="timeline-item">
+
+        @if ($step==1)
         <div class="timeline-icon">
             <img src="images/minusx40.png" alt="">
         </div>
         <div class="timeline-content">
-            <h2>  - </h2>
+            <h2> - </h2>
             <p>
-            ส่งเรื่องสำเร็จ
+                {{ trans('message.tx_status1') }}
             </p>
         </div>
-@endif
-</div>
-<div class="timeline-item">
-@if ($step==2) 
+        @endif
+    </div>
+    <div class="timeline-item">
+        @if ($step==2)
         <div class="timeline-icon">
-        <img src="images/minusx40.png" alt="">
+            <img src="images/minusx40.png" alt="">
         </div>
-            <div class="timeline-content">
-                <h2> - </h2>
-                <p>
-                เจ้าหน้าที่รับเรื่องแล้ว
-                </p>
-            </div>
-@endif
-</div>
-<div class="timeline-item">
-@if ($step==3 or $step==2) 
+        <div class="timeline-content">
+            <h2> - </h2>
+            <p>
+                {{ trans('message.tx_status2_1') }}
+            </p>
+        </div>
+        @endif
+    </div>
+    <div class="timeline-item">
+        @if ($step==3 or $step==2)
 
         <div class="timeline-icon">
-        <img src="images/minusx40.png" alt="">
+            <img src="images/minusx40.png" alt="">
         </div>
-            <div class="timeline-content">
-                <h2> - </h2>
-                <p>
-                 เจ้าหน้าที่ยังไม่ได้สอบถามข้อมูลเพิ่มเติม
-                </p>
-        
+        <div class="timeline-content">
+            <h2> - </h2>
+            <p>
+                {{ trans('message.tx_status3_1') }}
+            </p>
+
         </div>
-@endif
-</div>
-<div class="timeline-item">
-@if ($step==4 or $step==3 or $step==2) 
+        @endif
+    </div>
+    <div class="timeline-item">
+        @if ($step==4 or $step==3 or $step==2)
         <div class="timeline-icon">
-        <img src="images/minusx40.png" alt="">
+            <img src="images/minusx40.png" alt="">
         </div>
-            <div class="timeline-content">
-                <h2> - </h2>
-                <p>
-                อยู่ระหว่างดำเนินการช่วยเหลือ
-                </p>
+        <div class="timeline-content">
+            <h2> - </h2>
+            <p>
+                {{ trans('message.tx_status4_1') }}
+            </p>
         </div>
-@endif
-</div>
-<div class="timeline-item">
-@if ($step==5 or $step==4 or $step==3 or $step==2) 
+        @endif
+    </div>
+    <div class="timeline-item">
+        @if ($step==5 or $step==4 or $step==3 or $step==2)
         <div class="timeline-icon">
-        <img src="images/minusx40.png" alt="">
+            <img src="images/minusx40.png" alt="">
         </div>
-            <div class="timeline-content">
-                <h2> - </h2>
-                <p>
-                ดำเนินการเสร็จสิ้น
-                </p>
+        <div class="timeline-content">
+            <h2> - </h2>
+            <p>
+                {{ trans('message.tx_status5_1') }}
+            </p>
         </div>
-@endif
-</div>
+        @endif
+    </div>
 </div>
--- resources/views/layout/status.blade.php
+++ resources/views/layout/status.blade.php
@@ -5,19 +5,21 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="theme-color" content="#cc99cc"/>
-	<link href="{{ asset('bulma/css/bulma.css') }}" rel="stylesheet">
+
+	<meta name="theme-color" content="#ab3c3c" />
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+
+    <link href="{{ asset('bulma-0.8.0/css/bulma.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mystyles.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300&display=swap" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <link href="{{ asset('css/font-awesome5.0.6/css/fontawesome-all.css') }}" rel="stylesheet">
 	<link href="{{ asset('css/base.css') }}" rel="stylesheet">
-	<!--link href="{{ asset('css/font-awesome/css/font-awesome.css') }}" rel="stylesheet"-->
-	<link href="{{ asset('css/font-awesome5.0.6/css/fontawesome-all.css') }}" rel="stylesheet">
-	<script src="{{ asset('css/jquery.min.js') }}"></script>
-	<!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-	<script src="http://bulma.io/vendor/clipboard-1.7.1.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script src="http://bulma.io/lib/main.js"></script-->
-	<script src="{{ asset('bulma/clipboard-1.7.1.min.js') }}"></script>
+
 	<script src="{{ asset('bulma/main.js') }}"></script>
+
 	<title> ปกป้อง (CRS) </title>
+
 </head>
 
 <body>
@@ -35,19 +37,54 @@
 	</section>
 
 
-	<section class="section">
+	<div class="navbar has-background-light">
+		<div class="navbar-end has-text-right">
+			<div class="navbar-item">
+
+				@if(Config::get('app.locale') == 'en')
+
+				<a class="button is-danger is-inverted is-rounded is-small" href="{{ URL::to('change/th') }}"> Thai
+					Site&nbsp;
+					<span class="fa-stack fa-1x">
+						<i class="fa fa-circle fa-stack-2x"></i>
+						<i class="fa fa-flag fa-stack-1x fa-inverse"></i>
+					</span>
+				</a>
+
+				@elseif(Config::get('app.locale') == 'th')
+
+				<a class="button is-danger is-inverted is-rounded is-small" href="{{ URL::to('change/en') }}">
+					English
+					Site&nbsp;
+					<span class="fa-stack fa-1x">
+						<i class="fa fa-circle fa-stack-2x"></i>
+						<i class="fa fa-flag fa-stack fa-inverse"></i>
+					</span>
+				</a>
+
+				@endif
+
+			</div>
+		</div>
+	</div>
+
+	<br>
+
+	
 		<div class="container">
 
 			<nav class="breadcrumb">
-				<ul>
-					<li><a href="{{ 'index.php' }}"><span class="icon is-small"><i class="fa fa-home"></i></span><span> หน้าหลัก </span></a>
-					</li>
-					<li class="is-active"><a><span class="icon is-small"><i class="fa fa-search"></i></span><span> ตรวจสอบสถานะ </span></a>
-					</li>
-				</ul>
-			</nav>
-			
-			<h1 class="title"> ตรวจสอบสถานะ </h1>
+                <ul>
+                    <li><a href="{{ 'index.php' }}"><span class="icon is-small"><i class="fa fa-home"></i></span><span>
+                        {{ trans('message.nav_home') }} </span></a>
+                    </li>
+                    <li class="is-active"><a><span class="icon is-small"><i class="fa fa-search"></i></span><span>
+                    {{ trans('message.tx_head_status') }} </span></a>
+                    </li>
+                </ul>
+            </nav>
+
+            <h1 class="title"> {{ trans('message.tx_head_status') }} </h1>
 			
 			
 			<!-- Main container -->
@@ -55,7 +92,7 @@
 			  <!-- Left side -->
 			    <div class="level-left">
 					<div class="level-item">
-						<p class="subtitle is-5"> ใส่รหัสเพื่อตรวจสอบ </p>
+						<p class="subtitle is-5"> {{ trans('message.tx_track_status') }} </p>
 					</div>
 					<div class="level-item">
 						<div class="field has-addons">
@@ -63,7 +100,7 @@
 								<input class="input" type="text" id="case_search">
 							</p>
 							<p class="control">
-								<button class="button" onclick="report_status();"> ค้นหา </button>
+								<button class="button" onclick="report_status();"> {{ trans('message.bt_search_status') }} </button>
 							</p>
 						</div>
 					</div>
@@ -76,7 +113,7 @@
 							<span class="icon is-medium">
 								<i class="fa fa-smile"></i>
 							</span>
-							<span> ประเมินความพึงพอใจ </span>
+							<span> {{ trans('message.bt_rate_status') }} </span>
 						</a>
 					</div>
 			  	</div>
@@ -97,8 +134,6 @@
 		</div>
 
 
-	</section>
-
 
 	@extends('footer')
 	
@@ -106,54 +141,54 @@
 	<div id="modal-ter" class="modal">
 	  <div class="modal-background"></div>
 	  <div class="modal-card">
-		<header class="modal-card-head">
-		  <p class="modal-card-title"> ประเมินความพึงพอใจ </p>
-		  <button class="delete"></button>
-		</header>
-		<section class="modal-card-body">
-		  <div class="content">
-			<h4> ให้คะแนนตามความพอใจของท่าน </h4>
-			<table class="table">
-			  <thead>
-				<tr>
-				  <th><abbr title="Position"> ลำดับ </abbr></th>
-				  <th> หัวข้อ </th>
-				  <th><abbr title="Played"> น้อยที่สุด </abbr></th>
-				  <th><abbr title="Won"> น้อย </abbr></th>
-				  <th><abbr title="Drawn"> มาก </abbr></th>
-				  <th><abbr title="Lost">  มากที่สุด</abbr></th>
-				</tr>
-			  </thead>
-			  
-			  <tbody>
-				<tr>
-				  <th>1</th>
-				  <td><a href="#" > การรับเรื่องและติดต่อกลับ </a>
-				  </td>
-				  <td>{{ Form::radio('eval1', '1' , true) }}</td>
-				  <td>{{ Form::radio('eval1', '2' , false) }}</td>
-				  <td>{{ Form::radio('eval1', '3' , false) }}</td>
-				  <td>{{ Form::radio('eval1', '4' , false) }}</td>
-				</tr>
-				<tr>
-				  <th>2</th>
-				  <td><a href="#" > การให้การช่วยเหลือ </a></td>
-					<td>{{ Form::radio('eval2', '1' , true) }}</td>
-					<td>{{ Form::radio('eval2', '2' , false) }}</td>
-					<td>{{ Form::radio('eval2', '3' , false) }}</td>
-					<td>{{ Form::radio('eval2', '4' , false) }}</td>
+            <header class="modal-card-head">
+                <p class="modal-card-title"> {{ trans('message.bt_rate_status') }} </p>
+                <button class="delete"></button>
+            </header>
+            <section class="modal-card-body">
+                <div class="content">
+                    <h4> {{ trans('message.bt_rate_status_head') }} </h4>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th><abbr title="Position"> {{ trans('message.bt_rate_status_id') }} </abbr></th>
+                                <th> {{ trans('message.bt_rate_status_topic') }} </th>
+                                <th><abbr title="Played"> {{ trans('message.bt_rate_status_sc1') }}  </abbr></th>
+                                <th><abbr title="Won"> {{ trans('message.bt_rate_status_sc2') }}  </abbr></th>
+                                <th><abbr title="Drawn"> {{ trans('message.bt_rate_status_sc3') }}  </abbr></th>
+                                <th><abbr title="Lost"> {{ trans('message.bt_rate_status_sc4') }} </abbr></th>
+                            </tr>
+                        </thead>
 
-				</tr>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td><a href="#"> {{ trans('message.bt_rate_status_topic1') }} </a>
+                                </td>
+                                <td>{{ Form::radio('eval1', '1' , true) }}</td>
+                                <td>{{ Form::radio('eval1', '2' , false) }}</td>
+                                <td>{{ Form::radio('eval1', '3' , false) }}</td>
+                                <td>{{ Form::radio('eval1', '4' , false) }}</td>
+                            </tr>
+                            <tr>
+                                <th>2</th>
+                                <td><a href="#"> {{ trans('message.bt_rate_status_topic2') }} </a></td>
+                                <td>{{ Form::radio('eval2', '1' , true) }}</td>
+                                <td>{{ Form::radio('eval2', '2' , false) }}</td>
+                                <td>{{ Form::radio('eval2', '3' , false) }}</td>
+                                <td>{{ Form::radio('eval2', '4' , false) }}</td>
 
-			  </tbody>
-			</table>
-		  </div>
-		</section>
-		<footer class="modal-card-foot">
-		  <a class="button is-success" onclick="evaluate1();"> บันทึก </a>
-		  <a class="button"> ยกเลิก </a>
-		</footer>
-	  </div>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            <footer class="modal-card-foot">
+                <a class="button is-success" onclick="evaluate1();"> {{ trans('message.bt_submit') }} </a>
+                <a class="button"> {{ trans('message.bt_cancle2') }}  </a>
+            </footer>
+        </div>
 	</div>
 
 
@@ -192,14 +227,16 @@
        // alert(case_id);
         var url = "{{route('case.status',['case_id' => ":case_id"]) }}";
         url = url.replace(':case_id', case_id);
-        console.log(url);
+        
+		console.log(url);
+		
         var $request = $.get(url); // make request
         var $container = $('#timeline');
+
         $request.done(function(data) { // success
             $container.html(data.html);
         }
-        )
-        ;
+        );
 
 	}
 
