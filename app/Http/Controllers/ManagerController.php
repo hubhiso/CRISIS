3d6fd219ff13e930e695f9605879f83aead11b64
Noppon Kumpdetch <>
2021-11-16 15:51:02 +0700

    update	- register page 		- guide page 		- officer input case page

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -10,11 +10,9 @@ use App\case_input;
 use App\timeline;
 use App\officer;
 use App\province;
-
-
+use App\amphur;
+use App\casetransfer;
 use Auth;
-
-
 
 class ManagerController extends Controller
 {
@@ -52,7 +50,8 @@ class ManagerController extends Controller
 
     function  load_register(){
         $provinces = province::orderBy('PROVINCE_NAME', 'asc')->get();
-        return view('Manager.create_officer',compact('provinces'));
+        $ck_officer = officer::all();
+        return view('Manager.create_officer',compact('provinces','ck_officer'));
     }
     protected function validator(array $data)
     {
@@ -63,6 +62,57 @@ class ManagerController extends Controller
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
+
+    public function ajax_amphur($prov_id) {
+        $prov_code    = $prov_id;
+        $new_prov_id = province::where('PROVINCE_CODE', '=', $prov_code)->first();
+        $category = amphur::where('PROVINCE_ID', '=', $new_prov_id->PROVINCE_ID)->get();
+        return response()->json($category);
+    }
+
+    public function ajax_email($email) {
+        
+        
+        $regex = "/^([a-zA-Z0-9\.]+@+[a-zA-Z]+(\.)+[a-zA-Z]{2,3})$/";
+
+        if(preg_match($regex,$email)){
+
+            $ck_officer = officer::where('email', '=', $email)->first();
+
+            if(!$ck_officer){
+                $ck_email = 1;
+            }else{
+                $ck_email = 0;
+            }
+        }else{
+            $ck_email = 2;
+        }
+        return response()->json($ck_email);
+       
+    }
+
+    public function ajax_tel($tel) {
+
+        $len_tel = strlen($tel);
+
+        $regex = '/^[0-9]*$/';
+
+        if($len_tel >= 9 and $len_tel <= 10 and preg_match($regex,$tel)){
+        
+            $ck_officer = officer::where('tel', '=', $tel)->first();
+
+            if(!$ck_officer){
+                $ck_tel = 1;
+            }else{
+                $ck_tel = 0;
+            }
+        }else{
+            $ck_tel = 2;
+        }
+       
+        return response()->json($ck_tel);
+    }
+    
     protected function create(array $data)
     {
 
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,16 @@ Route::prefix('manager')->group(function (){
     Route::get('/transfer_case/{case_id}', 'ManagerController@transfer')->name('manager.transfer_frm');
     Route::get('/register', 'ManagerController@load_register')->name('manager.register');
 
+    Route::get('ajax-email/{email}','ManagerController@ajax_email');
+    Route::get('ajax-tel/{tel}','ManagerController@ajax_tel');
+
     Route::post('/reject_cfm', 'ManagerController@reject_cfm')->name('manager.reject_cfm');
     Route::post('/transfer_cfm', 'ManagerController@transfer_cfm')->name('manager.transfer_cfm');
     Route::post('/register_cfm', 'ManagerController@create_officer')->name('manager.register_cfm');
+
+    Route::prefix('transfer_case')->group(function (){
+        Route::get('ajax-amphur/{prov_id}','ManagerController@ajax_amphur');
+    });
 });
 Route::prefix('officer')->group(function () {
     Route::get('/', 'OfficerController@index')->name('officer.main');
