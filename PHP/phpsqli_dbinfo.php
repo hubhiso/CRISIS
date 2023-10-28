1872f2b8dce9c62861a4a415ce0f48f026ad67cb
B <noppon.kumpdetch@gmail.com>
2023-10-29 02:18:19 +0700

    update php

--- PHP/phpsqli_dbinfo.php
+++ PHP/phpsqli_dbinfo.php
@@ -4,31 +4,13 @@ $password="hiso";
 $database="crisis";
 $hostname = "localhost";
 
-$mysqli = new mysqli($hostname,$username,$password,$database);
+$conn = mysqli_connect($hostname, $username, $password, $database);
 
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-
-// Return name of current default database
-if ($result = $mysqli -> query("SELECT DATABASE()")) {
-  $row = $result -> fetch_row();
-  //echo "Default database is " . $row[0];
-  $result -> close();
-}
-
-// Change db to "test" db
-$mysqli -> select_db("test");
-
-// Return name of current default database
-if ($result = $mysqli -> query("SELECT DATABASE()")) {
-  $row = $result -> fetch_row();
-  //echo "Default database is " . $row[0];
-  $result -> close();
-}
-
-$mysqli -> close();
-
+    if (mysqli_connect_errno()) 
+    { 
+        echo "Database connection failed."; 
+    }
+        // Change character set to utf8
+     mysqli_set_charset($conn,"utf8");
 
 ?>
