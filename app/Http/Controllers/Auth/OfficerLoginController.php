3c52ee936963f4d0eef65fc5600398b074bb349e
B <noppon.kumpdetch@gmail.com>
2023-10-31 15:50:39 +0700

    fix bug

--- app/Http/Controllers/Auth/OfficerLoginController.php
+++ app/Http/Controllers/Auth/OfficerLoginController.php
@@ -30,21 +30,55 @@ class OfficerLoginController extends Controller
             'password' => 'required|string',
         ]);
 
-        if( Auth::guard('officer')->attempt(['username' => $request->username , 'password' => $request->password, 'active' => 'yes' ]
-            , $request->remember)){
+        /*
+        
 
-            officer::where('username','=', $request->username)->update(['last_login_at' => Carbon::now()]);
-            return redirect()->intended(route('officer.main'));
-        }
-        
-        
         if( Auth::guard('officer')->attempt(['username' => $request->username , 'password' => $request->password , 'active' => 'no']))
         {
             Auth::guard('officer')->logout();
             return redirect()->back()->with(['message' => 'ID นี้ถูกระงับชั่วคราวจากการที่ไม่ได้ login เป็นเวลานาน โปรดแจ้งผู้ดูแลเพื่อเข้าใช้งาน']);
+        }else{
+
+            if( Auth::guard('officer')->attempt(['username' => $request->username , 'password' => $request->password ]
+           , $request->remember)){
+
+            officer::where('username','=', $request->username)->update(['last_login_at' => Carbon::now()]);
+            return redirect()->intended(route('officer.main'));
         }
 
-        return redirect()->back()->withInput($request->only('username','remember'));
+        }*/
+
+        if(Auth::guard('officer')->attempt(['username' => $request->username , 'password' => $request->password , 'active' => 'no' , 'approv' => 'no'])){
+
+            Auth::guard('officer')->logout();
+            return redirect()->back()->with(['message' => 'ID นี้กำลังรอการอนุมัติจากเจ้าหน้าที่ดูแลระบบ']);
+
+        }else if(Auth::guard('officer')->attempt(['username' => $request->username , 'password' => $request->password , 'active' => 'no' , 'approv' => NULL])){
+
+            Auth::guard('officer')->logout();
+            return redirect()->back()->with(['message' => 'ID นี้กำลังรอการอนุมัติจากเจ้าหน้าที่ดูแลระบบ']);
+
+        }else{
+            
+            if( Auth::guard('officer')->attempt(['username' => $request->username , 'password' => $request->password , 'active' => 'no']))
+            {
+                Auth::guard('officer')->logout();
+                return redirect()->back()->with(['message' => 'ID นี้ถูกระงับชั่วคราวจากการที่ไม่ได้ login เป็นเวลานาน โปรดแจ้งผู้ดูแลเพื่อเข้าใช้งาน']);
+            }else{
+
+                if( Auth::guard('officer')->attempt(['username' => $request->username , 'password' => $request->password ]
+                , $request->remember)){
+
+                    officer::where('username','=', $request->username)->update(['last_login_at' => Carbon::now()]);
+                    return redirect()->intended(route('officer.main'));
+                }
+
+            }
+        }
+
+
+
+       return redirect()->back()->withInput($request->only('username','remember'));
     }
 
     public  function  logout()
